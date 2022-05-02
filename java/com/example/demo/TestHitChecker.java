ce3bed4f9976c6fd0adbadb98b41113f5d2ca285
Artyom Artyomkin <welllet@mail.ru>
2022-05-02 21:38:33 +0300

    kakuyu-to huinyu namutil

--- java/com/example/demo/TestHitChecker.java
+++ java/com/example/demo/TestHitChecker.java
@@ -1,18 +1,16 @@
 package com.example.demo;
 
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
-public class TestHitChecker {
+import org.junit.*;
+import static org.junit.Assert.*;
+public class TestHitChecker extends Assert{
+    @Before
+    public void setUp() {
+        System.out.println("HI");
+    }
+ 
     @Test
     public void testHitChecker() {
-//        DotBean dot1 = new DotBean(1,1,1);
-//        DotBean dot2 = new DotBean(-0.5,1,1);
-//        DotBean dot3 = new DotBean(1,-1,1);
-//        DotBean dot4 = new DotBean(-0.4,-0.5,1);
-//        assertEquals(false, HitChecker.checkHit(dot1));
-//        assertEquals(false, HitChecker.checkHit(dot2));
-//        assertEquals(false, HitChecker.checkHit(dot3));
-//        assertEquals(true, HitChecker.checkHit(dot4));
-        assertEquals(true,true);
+         assertEquals(true,true);
     }
+    
 }
